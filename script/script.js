5a15b0e9c19dd83d7f4a849364d0eef6f51eeec0
Nastia_Chulupyn <nculupin@gmail.com>
2022-03-22 17:31:46 +0200

    Added slider

--- script/script.js
+++ script/script.js
@@ -1,29 +1,31 @@
-let swiperLength = document.getElementsByClassName("war_info__slider").length;
-if (swiperLength > 0) {
-  var swiper = new Swiper(".war_info__slider", {
-    watchOverflow: true,
-    loop: true,
-    speed: 1500,
-    autoplay: {
-      delay: 3500,
-      pauseOnMouseEnter: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
+document.addEventListener("DOMContentLoaded", function (event) {
+  let swiperLength = document.getElementsByClassName("war_info__slider").length;
+  if (swiperLength > 0) {
+    var swiper = new Swiper(".war_info__slider", {
+      watchOverflow: true,
+      loop: true,
+      speed: 3500,
+      autoplay: {
+        delay: 0,
       },
+      spaceBetween: 10,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
 
-      700: {
-        slidesPerView: 2,
-      },
+        700: {
+          slidesPerView: 2,
+        },
 
-      900: {
-        slidesPerView: 3,
-      },
+        900: {
+          slidesPerView: 3,
+        },
 
-      1280: {
-        slidesPerView: 4,
+        1280: {
+          slidesPerView: 4,
+        },
       },
-    },
-  });
-}
+    });
+  }
+});
